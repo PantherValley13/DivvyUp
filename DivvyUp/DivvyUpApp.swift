80e958b2c77fa6ee59eba3289f947fcab0dfbb1c
PantherValley13 <churchdarius@gmail.com>
2025-07-04 14:18:11 -0400

    Initial Commit

---
+++ DivvyUp/DivvyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivvyUpApp.swift
+//  DivvyUp
+//
+//  Created by Darius Church on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DivvyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DivvyUpTests/DivvyUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  DivvyUpTests.swift
+//  DivvyUpTests
+//
+//  Created by Darius Church on 7/4/25.
+//
+
+import Testing
+@testable import DivvyUp
+
+struct DivvyUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
